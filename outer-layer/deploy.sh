b929ec7fed521b4bb61a26991e25858ec9c170be
Markus Hinz <markus.hinz@tum.de>
2019-03-03 22:05:43 +0100

    deploy

--- outer-layer/deploy.sh
+++ outer-layer/deploy.sh
@@ -5,5 +5,5 @@ sudo -u $USER git pull
 sudo -u $USER rm -rf ./node_modules
 sudo -u $USER npm install
 
-systemctl restart socket-gateway-inner-layer
-systemctl status socket-gateway-inner-layer
+systemctl restart socket-gateway-outer-layer
+systemctl status socket-gateway-outer-layer
