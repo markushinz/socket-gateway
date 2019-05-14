49f10148b203edcac9b31a338deb52992b275e09
Markus Hinz <markus.hinz@tum.de>
2019-05-14 19:50:23 +0200

    css changes

--- outer-layer/deploy.sh
+++ outer-layer/deploy.sh
@@ -7,8 +7,9 @@ sudo -u $USER git pull
 sudo -u $USER rm -rf ./node_modules
 sudo -u $USER npm ci
 
-sudo -u $USER rm -f ./public/clr-ui.min.css
 sudo -u $USER cp ./node_modules/@clr/ui/clr-ui.min.css ./public/clr-ui.min.css
+sudo -u $USER cp ./node_modules/@clr/icons/clr-icons.min.css ./public/clr-icons.min.css
+sudo -u $USER cp ./node_modules/@clr/icons/clr-icons.min.js ./public/clr-icons.min.js
 
 # sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/bin/node
 
