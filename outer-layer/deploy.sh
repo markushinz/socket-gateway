022d628696c2376f6632afdb790ee6faf78657b0
Markus Hinz <markus.hinz@tum.de>
2019-07-24 23:59:31 +0200

    just cp

--- outer-layer/deploy.sh
+++ outer-layer/deploy.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
-branch=${1:-master}
-deploy=${2:-systemctl}
 
-sudo -u $USER git checkout $branch
-sudo -u $USER git pull
-sudo -u $USER rm -rf ./node_modules
-sudo -u $USER npm ci
-
-sudo -u $USER cp ./node_modules/@clr/ui/clr-ui.min.css ./public/clr-ui.min.css
-sudo -u $USER cp ./node_modules/@clr/ui/clr-ui.min.css.map ./public/clr-ui.min.css.map
-sudo -u $USER cp ./node_modules/@clr/icons/clr-icons.min.css ./public/clr-icons.min.css
-sudo -u $USER cp ./node_modules/@clr/icons/clr-icons.min.css.map ./public/clr-icons.min.css.map
-sudo -u $USER cp ./node_modules/@clr/icons/clr-icons.min.js ./public/clr-icons.min.js
-
-# sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/bin/node
-
-if [ $deploy = systemctl ]; then
-   systemctl restart socket-gateway-outer-layer
-   systemctl status socket-gateway-outer-layer
-else
-  sudo -u $USER npm start
-fi
+cp ./node_modules/@clr/ui/clr-ui.min.css ./public/clr-ui.min.css
+cp ./node_modules/@clr/ui/clr-ui.min.css.map ./public/clr-ui.min.css.map
+cp ./node_modules/@clr/icons/clr-icons.min.css ./public/clr-icons.min.css
+cp ./node_modules/@clr/icons/clr-icons.min.css.map ./public/clr-icons.min.css.map
+cp ./node_modules/@clr/icons/clr-icons.min.js ./public/clr-icons.min.js
