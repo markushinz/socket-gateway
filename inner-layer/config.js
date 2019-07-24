e27b8fb3bab952acfaf37c0fbeecc6de59da736c
Markus Hinz <markus.hinz@tum.de>
2019-07-24 20:31:01 +0200

    simplify inner

--- inner-layer/config.js
+++ inner-layer/config.js
@@ -4,9 +4,9 @@ module.exports = {
     outerLayer: process.env.OUTER_LAYER || 'https://localhost:3001',
 
     tlsOptions: {
-        cert: fs.readFileSync(process.env.INNER_LAYER_CERT || __dirname + '/config/innerLayer.crt'),
-        key: fs.readFileSync(process.env.INNER_LAYER_KEY || __dirname + '/config/innerLayer.key'),
-        ca: fs.readFileSync(process.env.OUTER_LAYER_CERT ||  __dirname + '/config/outerLayer.crt'),
+        cert: fs.readFileSync(__dirname + '/config/innerLayer.crt'),
+        key: fs.readFileSync(__dirname + '/config/innerLayer.key'),
+        ca: fs.readFileSync(__dirname + '/config/outerLayer.crt'),
         rejectUnauthorized: true
     }
 };
