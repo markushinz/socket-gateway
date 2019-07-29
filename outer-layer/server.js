3b7d22a3bde69e2fb9e89460b35ef57b023624f4
Markus Hinz <markus.hinz@tum.de>
2019-07-29 22:30:41 +0200

    redirection on port 80

--- outer-layer/server.js
+++ outer-layer/server.js
@@ -16,3 +16,23 @@ appServer.listen(config.appPort);
 console.log(`Listening on port ${config.appPort}...`);
 socketServer.listen(config.socketPort);
 console.log(`Awaiting connections from inner layer(s) on port ${config.socketPort}...`);
+
+if (config.appPort == 443) {
+    try {
+        const http = require('http');
+
+        const express = require('express');
+        const httpAPP = express();
+
+        httpAPP.get('*', function (req, res) {
+            res.redirect('https://' + req.headers.host + req.url);
+        });
+
+        const appHTTPServer = http.createServer(httpAPP);
+        appHTTPServer.listen(80);
+        console.log(`Listening on port 80 for redirection...`);
+    }
+    catch (error) {
+        // console.log(`Could not listen on port 80 for redirection.`);
+    }
+}
