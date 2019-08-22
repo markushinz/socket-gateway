778ece0d48fb869edaabb63b2048cecb860cdb7c
Markus Hinz <markus.hinz@tum.de>
2019-08-22 20:23:15 +0200

    allow ssl errors on nonprod

--- inner-layer/config.js
+++ inner-layer/config.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 
+if (process.env.NODE_ENV !== 'production') {
+    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+}
+
 module.exports = {
     outerLayer: process.env.OUTER_LAYER || 'https://localhost:3000',
 
