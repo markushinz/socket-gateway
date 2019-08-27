89f6002667f9302ea904eab813afbe4bebda14b9
Markus Hinz <markus.hinz@tum.de>
2019-08-28 01:01:36 +0200

    remove try

--- inner-layer/server.js
+++ inner-layer/server.js
@@ -12,40 +12,36 @@ io.on('connect', function () {
 });
 
 io.on('request', function (incomingData) {
-    try {
-        axios({
-            method: incomingData.method,
-            url: incomingData.url,
-            headers: incomingData.headers,
-            params: incomingData.query,
-            data: incomingData.body,
-            maxRedirects: 0,
-            responseType: 'arraybuffer',
-            responseEncoding: null,
-            validateStatus: null,
-        }).then(function (response) {
-            const outgoingData = {
-                uuid: incomingData.uuid,
-                host: incomingData.host,
-                statusCode: response.status,
-                body: response.data.toString('binary'),
-                headers: response.headers
-            }
-            io.emit('request', outgoingData);
-        }).catch(function (error) {
-            console.error(error);
-            const outgoingData = {
-                uuid: incomingData.uuid,
-                host: incomingData.host,
-                statusCode: 500,
-                body: '{ "message": "Internal Server Error" }',
-                headers: { 'Content-Type': 'application/json' }
-            }
-            io.emit('request', outgoingData);
-        });
-    } catch (error) {
+    axios({
+        method: incomingData.method,
+        url: incomingData.url,
+        headers: incomingData.headers,
+        params: incomingData.query,
+        data: incomingData.body,
+        maxRedirects: 0,
+        responseType: 'arraybuffer',
+        responseEncoding: null,
+        validateStatus: null,
+    }).then(function (response) {
+        const outgoingData = {
+            uuid: incomingData.uuid,
+            host: incomingData.host,
+            statusCode: response.status,
+            body: response.data.toString('binary'),
+            headers: response.headers
+        }
+        io.emit('request', outgoingData);
+    }).catch(function (error) {
         console.error(error);
-    }
+        const outgoingData = {
+            uuid: incomingData.uuid,
+            host: incomingData.host,
+            statusCode: 500,
+            body: '{ "message": "Internal Server Error" }',
+            headers: { 'Content-Type': 'application/json' }
+        }
+        io.emit('request', outgoingData);
+    });
 });
 
 io.on('customPing', function (incomingData) {
