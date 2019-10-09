25423664dee9b0e37bdb87242c50de0bbb79b280
Markus Hinz <markus.hinz@tum.de>
2019-10-09 20:52:51 +0200

    html

--- outer-layer/app.js
+++ outer-layer/app.js
@@ -54,23 +54,23 @@ app.get('/admin', function (req, res, next) {
             const innerLayers = app.get('gateway').innerLayers
             res.setHeader('content-type', 'text/html; charset=utf-8')
             res.send(`<!DOCTYPE html>
-            <html lang="en">
-            
-            <head>
-                <meta charset="utf-8">
-                <title>Socket Gateway</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-            </head>
-            
-            <body>
-                <h1>Socket Gateway</h1>
-                <h3>Inner Layers</h3>
-                <pre>${JSON.stringify(innerLayers, null, 4)}</pre>
-                <h3>Targets</h3>
-                <pre>${JSON.stringify(config.targets, null, 4)}</pre>
-            </body>
-            
-            </html>`);
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Socket Gateway</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+  
+<body>
+    <h1>Socket Gateway</h1>
+    <h3>Inner Layers</h3>
+    <pre>${JSON.stringify(innerLayers, null, 4)}</pre>
+    <h3>Targets</h3>
+    <pre>${JSON.stringify(config.targets, null, 4)}</pre>
+</body>
+
+</html>`);
         } else {
             res.setHeader('www-authenticate', 'Basic realm="Socket Gateway"')
             res.status(401).json({ message: 'Unauthorized' });
