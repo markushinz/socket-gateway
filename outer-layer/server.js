734dfd3dc975ace0785ec4d6bfac10bd65e6d79b
Markus Hinz <markus.hinz@tum.de>
2020-01-25 15:37:26 +0100

    rm console log

--- outer-layer/server.js
+++ outer-layer/server.js
@@ -6,7 +6,6 @@ const app = require('./app');
 const appServer = https.createServer(config.appTlsOptions, app);
 
 const socket = require('./socket');
-console.log(config.socketTlsOptions);
 const socketServer = https.createServer(config.socketTlsOptions);
 const gateway = socket.createGateway(socketServer);
 
