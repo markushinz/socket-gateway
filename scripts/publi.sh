8483a45fee3fc236aa9f64f97cf651861147287f
Markus Hinz <info@markushinz.de>
2021-05-30 21:52:01 +0200

    build multi-arch

--- scripts/publi.sh
+++ scripts/publi.sh
@@ -1,22 +1,10 @@
 #!/usr/bin/env bash
 
-set -e
+set -eo pipefail
 
 git diff --exit-code
 
-git fetch
-tag=$(git describe --abbrev=0)
-
-major=0
-minor=0
-patch=0
-if [[ ${tag:1} =~ ([0-9]+).([0-9]+).([0-9]+) ]]; then
-  major="${BASH_REMATCH[1]}"
-  minor="${BASH_REMATCH[2]}"
-  patch="${BASH_REMATCH[3]}"
-fi
-patch=$(echo "${patch} + 1" | bc)
-version=${1:-"${major}.${minor}.${patch}"}
+version=${1:-"$(./version.sh)"}
 
 echo "${version}"
 
