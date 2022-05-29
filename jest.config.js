9d5255f9972b495759e957f35382ae20ea500715
Markus Hinz <info@markushinz.de>
2022-05-29 14:29:18 +0200

    remove strict

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
     preset: 'ts-jest',
-    // silent: true,
+    silent: true,
     verbose: true,
     collectCoverage: true,
     testEnvironment: 'node'
